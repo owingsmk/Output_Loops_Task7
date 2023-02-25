cdd64ba22d0eda78ce57204ac6da5b9730ee9d61
Mark Owings <owingsmk@mail.uc.edu>
2023-02-25 15:05:54 -0500

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for(int i=0;i<5;i++)
-            System.out.println("***** ");
+        for(int i=0;i<5;i++) //looping the * to repeat itself 5 times
+            System.out.println("***** "); //pattern being used each time
     }
 }
